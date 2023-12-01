43e041eb1d5dc3e196878cb06a2959e1ce086a2d
crackanddie <abdrakovairat@gmail.com>
2023-12-01 13:59:23 +0300

    1.1.0 release

--- Abdrakov.CommonWPF/Properties/AssemblyInfo.cs
+++ Abdrakov.CommonWPF/Properties/AssemblyInfo.cs
@@ -3,37 +3,5 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows.Markup;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Abdrakov.Solutions")]
-[assembly: AssemblyDescription("A powerful base for MVVM apps with Prism")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Abdrakov corp.")]
-[assembly: AssemblyProduct("Abdrakov.Solutions")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("393abb40-3085-4cf0-9bca-f5fc70f075cd")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.10.0")]
-[assembly: AssemblyFileVersion("1.0.10.0")]
 
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "Abdrakov.CommonWPF.Localization")]
